ee167522c9ae2a6fc11257840cac180b009b99e8
dmitry_pilipovich <dzmitry.pilipovich@modsen-software.com>
2023-09-07 22:04:25 +0300

    feat: implemented the logic of invitations

---
+++ app/api/servers/[serverId]/invite-code/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
+
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!params.serverId) {
+      return new NextResponse("Bad Request", { status: 400 });
+    }
+
+    const { serverId } = params;
+
+    const existingServer = await db.server.findFirst({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+    });
+
+    if (existingServer) {
+      const updatedServer = await db.server.update({
+        where: {
+          id: serverId,
+        },
+        data: {
+          inviteCode: uuidv4(),
+        },
+      });
+
+      return NextResponse.json(updatedServer);
+    }
+
+    return new NextResponse("Access denied", { status: 403 });
+  } catch (err) {
+    console.log("[SERVER_ID]", err);
+
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -19,6 +19,7 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator
 } from "@/components/ui/dropdown-menu";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles,
@@ -26,6 +27,8 @@ interface ServerHeaderProps {
 }
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = useModal();
+
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
@@ -52,6 +55,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           <DropdownMenuItem
             className="text-indigo-600 dark:text-indigo-400
             px-3 py-2 text-sm cursor-pointer"
+            onClick={() => onOpen("invite", { server })}
           >
             Invite people
             <UserPlus className="h-4 w-4 ml-auto" />
